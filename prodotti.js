bcfcf666b78111f03b6831db4efce422be2307ab
Maral996 <alessiomarches@hotmail.it>
2026-02-09 21:44:22 +0100

    Update prodotti.js

--- prodotti.js
+++ prodotti.js
@@ -1,18 +1,40 @@
-// Database centrale dei prodotti
-const LISTA_PRODOTTI = { 
-    1: { 
+
+
+const LISTA_PRODOTTI = {
+    R1: { 
         nome: "Spazzole Tergicristallo", 
         prezzo: 30, 
-        immagine: " " 
+        categoria: "Ricambi", 
+        immagine: "img/spazzole.jpg", 
+        desc: "Set completo anteriore" 
+    },
+    R2: { 
+        nome: "Batteria xAh", 
+        prezzo: 75,
+        categoria: "Ricambi",
+        immagine: " "
+        desc: "Batteria xah ymh"
     }, 
-    2: { 
-        nome: "Batteria 60Ah", 
-        prezzo: 75, 
-        immagine: " " 
-    }, 
-    3: { 
+    
+    R3: { 
         nome: "Lampadina Bosch H7", 
         prezzo: 12, 
-        immagine: "img/lampadinah7bosch.jpg" // Senza immagine: non apparirà nulla nel box
+        immagine: "img/lampadinah7bosch.jpg"
+    },
+    
+    
+    P1: { 
+        nome: "Pneumatico Michelin 205/55 R16", 
+        prezzo: 85.50, 
+        categoria: "Pneumatici", 
+        immagine: "img/michelin.jpg", 
+        desc: "Estivo, ottima tenuta" 
+    },
+    G1: { 
+        nome: "Profumatore Car's World", 
+        prezzo: 5, 
+        categoria: "Gadget", 
+        immagine: "img/profumo.jpg", 
+        desc: "Fragranza pino" 
     }
 };
